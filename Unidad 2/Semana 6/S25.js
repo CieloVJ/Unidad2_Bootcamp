adb62f7cfba2c392bcff82f8ef25a9ce3d064ec3
CieloVJ <cielovj243@gmail.com>
2021-11-17 17:40:34 -0500

    Unidad 2

---
+++ Unidad 2/Semana 6/S25.js
@@ -0,0 +1,66 @@
+/*let = 0
+while (let <= 100){
+    console.log= let + 2
+}*/
+
+/*number = 1
+while (number <= 12){
+    console.log(number);
+    number = number + 2
+}*/
+
+let cat = 1
+/*while (cat <= 12){
+    console.log(cat);
+    cat = cat + 1
+}
+*/
+/*do {
+    console.log('Hello world');
+  } while (2 > 3);*/
+/*
+console.log("Hola" + "mundo");*/
+
+/*for (let number = 1; number < 8; number = number * 3) {
+    console.log(number);
+  }*/
+
+  /*const weather = 'soleado';
+
+  switch (weather) {
+    case 'lluvioso':
+      console.log('Recuerda levar un paraguas.');
+      break;
+    case 'soleado':
+      console.log('Viste ligero.');
+    case 'nublado':
+      console.log('Sal a la calle');
+  
+    default:
+      console.log('Tipo de clima desconocido');
+      break;
+  }*/
+
+  /*for (let i = 0; i <= 10; i++) {
+    if (i == 0) {
+      continue;
+    } else {
+      console.log(i);
+    }
+  }*/
+
+ /* var medalForScore = function(score) {
+    if (score < 3){
+      return 'Bronze';
+    }
+    if (score < 7){
+      return 'silver';
+    }
+    return 'Gold';
+  }
+  
+  console.log(medalForScore(3));*/
+
+  while (3 > 2) {
+    console.log('Hello world');
+  }
